8fed1cc5d2f61199142155e4f63735d9a37acffb
Quadrifoglio <quadrifoglio.clement@gmail.com>
2015-08-30 13:46:01 +0200

    EBML header parsing done

--- ebml/elements.go
+++ ebml/elements.go
@@ -10,8 +10,34 @@ const (
 	ElementDocTypeVersion     uint32 = 0x4287
 	ElementDocTypeReadVersion uint32 = 0x4285
 
-	ElementVoid  uint32 = 0xec
-	ElementCRC32 uint32 = 0xbf
+	ElementVoid uint32 = 0xec
 
 	ElementSegment uint32 = 0x18538067
 )
+
+func GetElementName(id uint32) string {
+	switch id {
+	case ElementEBML:
+		return "EBML"
+	case ElementEBMLVersion:
+		return "EBMLVersion"
+	case ElementEBMLReadVersion:
+		return "EBMLReadVersion"
+	case ElementEBMLMaxIDLength:
+		return "EBMLMaxIDLength"
+	case ElementEBMLMaxSizeLength:
+		return "EBMLMaxSizeLength"
+	case ElementDocType:
+		return "DocType"
+	case ElementDocTypeVersion:
+		return "DocTypeVersion"
+	case ElementDocTypeReadVersion:
+		return "DocTypeReadVersion"
+	case ElementVoid:
+		return "Void"
+	case ElementSegment:
+		return "Segment"
+	default:
+		return "Unknown"
+	}
+}
--- ebml/parser.go
+++ ebml/parser.go
@@ -18,9 +18,10 @@ const (
 )
 
 type Element struct {
+	ID    uint32
 	Level uint8
 	Type  uint8
-	Size  uint
+	Size  uint64
 	Data  []byte
 }
 
@@ -37,13 +38,29 @@ func Parse(filename string) error {
 	var cursor uint64 = 0
 	var i int = 0
 
+	var level = 0
+	var levelEnd uint64 = 0
+
 	for cursor < uint64(len(data)) {
-		_, err := getNextElement(data, &cursor)
+		el, err := getNextElement(data, &cursor)
 		if err != nil {
-			cursor++
-			continue
+			fmt.Printf("Finished at index %d (value 0x%x): %s\n", cursor, data[cursor], err)
+			break
 		}
 
+		if levelEnd != 0 && cursor == levelEnd {
+			level--
+			levelEnd = 0
+		}
+
+		el.Level = uint8(level)
+
+		if el.Type == TypeMasterElement {
+			level++
+			levelEnd = cursor + el.Size
+		}
+
+		fmt.Printf("%d: %s (0x%d) of size %d - Value: %v\n", el.Level, GetElementName(el.ID), el.ID, el.Size, el.Data)
 		i++
 	}
 
@@ -52,97 +69,207 @@ func Parse(filename string) error {
 
 func getNextElement(data []byte, cursor *uint64) (Element, error) {
 	var res Element
+	if *cursor >= uint64(len(data)) {
+		return res, io.EOF
+	}
+
 	var b = data[*cursor]
 
 	if ((b & 0x80) >> 7) == 1 { // Class A ID (on 1 byte)
-		*cursor += 1
-		b = data[*cursor]
+		t, err := getElementID(1, data, cursor)
+		switch t {
+		}
 
-		size := getElementSize(data, cursor)
-		fmt.Println("Class A of size", size)
+		_, err = getElementSize(data, cursor)
+		if err != nil {
+			return res, err
+		}
+
 		return res, nil
 	}
-	if ((b & 0x40) >> 6) == 1 {
-		*cursor += 2
-		b = data[*cursor]
+	if ((b & 0x40) >> 6) == 1 { // Class B ID (on 2 byte)
+		id, err := getElementID(2, data, cursor)
+		if err != nil {
+			return res, err
+		}
 
-		size := getElementSize(data, cursor)
-		fmt.Println("Class B of size", size)
+		size, err := getElementSize(data, cursor)
+		if err != nil {
+			return res, err
+		}
+
+		switch id {
+		case ElementEBMLVersion:
+			res.Type = TypeUint
+			break
+		case ElementEBMLReadVersion:
+			res.Type = TypeUint
+			break
+		case ElementEBMLMaxIDLength:
+			res.Type = TypeUint
+			break
+		case ElementEBMLMaxSizeLength:
+			res.Type = TypeUint
+			break
+		case ElementDocType:
+			res.Type = TypeString
+			break
+		case ElementDocTypeVersion:
+			res.Type = TypeUint
+			break
+		case ElementDocTypeReadVersion:
+			res.Type = TypeUint
+			break
+		}
+
+		d, err := getElementData(size, data, cursor)
+		if err != nil {
+			return res, nil
+		}
+
+		res.ID = id
+		res.Data = d
+		res.Size = size
 		return res, nil
 	}
-	if ((b & 0x20) >> 5) == 1 {
-		*cursor += 3
-		b = data[*cursor]
+	if ((b & 0x20) >> 5) == 1 { // Class C ID (on 4 bytes)
+		id, err := getElementID(3, data, cursor)
 
-		size := getElementSize(data, cursor)
-		fmt.Println("Class C of size", size)
+		switch id {
+		}
+
+		size, err := getElementSize(data, cursor)
+		if err != nil {
+			return res, err
+		}
+
+		res.ID = id
+		res.Size = size
 		return res, nil
 	}
-	if ((b & 0x10) >> 4) == 1 {
-		*cursor += 4
-		b = data[*cursor]
+	if ((b & 0x10) >> 4) == 1 { // Class D ID (on 4 bytes)
+		id, err := getElementID(4, data, cursor)
 
-		size := getElementSize(data, cursor)
-		fmt.Println("Class D of size", size)
+		switch id {
+		case ElementEBML:
+			res.Type = TypeMasterElement
+			break
+		case ElementSegment:
+			res.Type = TypeMasterElement
+			break
+		}
+
+		size, err := getElementSize(data, cursor)
+		if err != nil {
+			return res, err
+		}
+
+		res.ID = id
+		res.Size = size
 		return res, nil
 	}
 
-	return res, io.EOF
+	return res, fmt.Errorf("Failed to identify tag")
 }
 
-func getElementSize(data []byte, at *uint64) uint64 {
+func getElementID(class uint8, data []byte, at *uint64) (uint32, error) {
+	if class == 1 {
+		b := data[*at]
+
+		*at++
+		return uint32(b), nil
+	}
+	if class == 2 {
+		b := data[*at : *at+2]
+
+		*at += 2
+		return uint32(pack16(b)), nil
+	}
+	if class == 3 {
+		b := data[*at : *at+3]
+
+		*at += 3
+		return uint32(pack24(b)), nil
+	}
+	if class == 4 {
+		b := data[*at : *at+4]
+
+		*at += 4
+		return uint32(pack32(b)), nil
+	}
+
+	return 0, fmt.Errorf("Unknown element")
+}
+
+func getElementSize(data []byte, at *uint64) (uint64, error) {
 	b := data[*at]
 
 	if ((b & 0x80) >> 7) == 1 { // Size coded on 1 byte
 		v := uint64(b & 0x7f)
 
 		*at++
-		return v
+		return v, nil
 	}
 	if ((b & 0x40) >> 6) == 1 { // Size coded on 2 byte
 		v := uint64(pack16([]byte{0x3f & b, data[*at+1]}))
 
 		*at += 2
-		return v
+		return v, nil
 	}
 	if ((b & 0x20) >> 5) == 1 { // Size coded on 3 byte
 		v := uint64(pack24([]byte{0x1f & b, data[*at+1], data[*at+2]}))
 
 		*at += 3
-		return v
+		return v, nil
 	}
 	if ((b & 0x10) >> 4) == 1 { // Size coded on 4 byte
 		v := uint64(pack32([]byte{0xf & b, data[*at+1], data[*at+2], data[*at+3]}))
 
 		*at += 4
-		return v
+		return v, nil
 	}
 	if ((b & 0x8) >> 3) == 1 { // Size coded on 5 byte
 		v := uint64(pack40([]byte{0x7 & b, data[*at+1], data[*at+2], data[*at+3], data[*at+4]}))
 
 		*at += 5
-		return v
+		return v, nil
 	}
 	if ((b & 0x4) >> 2) == 1 { // Size coded on 6 byte
 		v := uint64(pack48([]byte{0x3 & b, data[*at+1], data[*at+2], data[*at+3], data[*at+4], data[*at+5]}))
 
 		*at += 6
-		return v
+		return v, nil
 	}
 	if ((b & 0x2) >> 1) == 1 { // Size coded on 7 byte
 		v := uint64(pack56([]byte{0x1 & b, data[*at+1], data[*at+2], data[*at+3], data[*at+4], data[*at+5], data[*at+6]}))
 
 		*at += 7
-		return v
+		return v, nil
 	}
 	if ((b & 0x1) >> 0) == 1 { // Size coded on 8 byte
 		v := uint64(pack64([]byte{0x0 & b, data[*at+1], data[*at+2], data[*at+3], data[*at+4], data[*at+5], data[*at+6], data[*at+7]}))
 
 		*at += 8
-		return v
+		return v, nil
 	}
 
-	return 0
+	return 0, fmt.Errorf("Invalid size format")
+}
+
+func getElementData(size uint64, b []byte, at *uint64) ([]byte, error) {
+	if uint64(len(b[*at:*at+size])) != size {
+		return nil, io.EOF
+	}
+
+	var i uint64
+	var buffer = make([]byte, size)
+
+	for i = 0; i < size; i++ {
+		buffer[i] = b[i]
+	}
+
+	*at += size
+	return buffer, nil
 }
 
 func pack16(b []byte) uint16 {
